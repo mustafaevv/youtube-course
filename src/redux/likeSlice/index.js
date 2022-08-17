0922a2a74bac4a3fd2529c8be4f1a4003db836d9
Mustafaev.Ubaydullo <78743719+mustafaevv@users.noreply.github.com>
2022-08-17 19:37:25 +0500

    Update index.js

--- src/redux/likeSlice/index.js
+++ src/redux/likeSlice/index.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = JSON.parse(localStorage.getItem('favorite') || '[]')
+const initialState = JSON.parse(localStorage.getItem('favorite') || '{}')
 
 export const likeSlice = createSlice({
   name: 'favorite',
